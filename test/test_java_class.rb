91fead9f9c1aaa1efab8d8d0feb7f86ed595e2dd
headius@cnutter.local <none@none>
2008-10-27 00:48:50 -0500

    Add some JavaClass.java_method tests

--- test/test_java_class.rb
+++ test/test_java_class.rb
@@ -13,7 +13,7 @@ class TestJavaClass < Test::Unit::TestCase
     assert_not_nil cons1
     assert_not_nil cons2
 
-    cls = JVMScript::FileBuilder.new('x').public_class("y")
+    cls = JVMScript::FileBuilder.new('x').public_class('y')
     cls.public_constructor()
     cls.public_constructor(java.lang.String.java_class)
 
@@ -22,5 +22,29 @@ class TestJavaClass < Test::Unit::TestCase
 
     assert_not_nil cons1
     assert_not_nil cons2
+  end
+
+  def test_java_method
+    cls = java.lang.String.java_class
+    
+    m1 = cls.java_method("toString")
+    m2 = cls.java_method("equals", java.lang.Object.java_class)
+
+    assert_not_nil m1
+    assert_not_nil m2
+    assert_equal java.lang.String.java_class, m1.return_type
+    assert_equal Java::boolean.java_class, m2.return_type
+
+    cls = JVMScript::FileBuilder.new('x').public_class('y')
+    cls.public_method('toString', java.lang.String.java_class)
+    cls.public_method('equals', Java::boolean.java_class, java.lang.Object.java_class)
+
+    m1 = cls.java_method('toString')
+    m2 = cls.java_method('equals', java.lang.Object.java_class)
+    
+    assert_not_nil m1
+    assert_not_nil m2
+    assert_equal java.lang.String.java_class, m1.return_type
+    assert_equal Java::boolean.java_class, m2.return_type
   end
 end
