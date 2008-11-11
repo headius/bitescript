b00473d01337e5a0efe6e3b64afdf52e1e3d1c50
headius@cnutter.local <none@none>
2008-11-10 22:00:02 -0600

    Clean up tests, add some more "real" assertions based on loading and calling the generated classes.

--- test/test_builder.rb
+++ test/test_builder.rb
@@ -3,46 +3,81 @@ require 'jvmscript/builder'
 
 class TestBuilder < Test::Unit::TestCase
   import java.util.ArrayList
+  import java.lang.System
   
   def setup
     @builder = JVMScript::FileBuilder.build('somefile.source')
+    @class_name = "Foo" + System.current_time_millis.to_s
+  end
+
+  def dummy_constructor(class_builder)
+    class_builder.public_constructor do
+      aload local 'this'
+      invokespecial object, '<init>', [void]
+      returnvoid
+    end
+  end
+
+  def load_and_construct(name, class_builder)
+    class_bytes = class_builder.generate
+    cls = JRuby.runtime.jruby_class_loader.define_class(name, class_bytes.to_java_bytes)
+
+    cls.new_instance
   end
 
   def test_instance_method_this
-    cb = @builder.public_class('Foo', @builder.object);
-    method = cb.public_method("foo", @builder.void)
+    cb = @builder.public_class(@class_name, @builder.object);
+    method = cb.public_method("foo", cb.this) {aload local 'this'; areturn}
 
     # ensure "this" is taking slot zero
     method.local('another')
     assert_equal(method.local('this'), 0)
     assert_equal(method.local('another'), 1)
+
+    dummy_constructor(cb)
+    obj = load_and_construct(@class_name, cb)
+
+    assert_equal(obj, obj.foo)
   end
 
   def test_constructor_this
-    cb = @builder.public_class('Foo', @builder.object);
-    constructor = cb.public_constructor
+    cb = @builder.public_class(@class_name, @builder.object);
+    cb.private_field "self", cb.this
+    constructor = cb.public_constructor do
+      aload 0
+      dup
+      invokespecial object, "<init>", [void]
+      dup
+      putfield this, "self", this
+      returnvoid
+    end
+
+    cb.public_method "get_self", cb.this do
+      aload 0
+      getfield this, "self", this
+      areturn
+    end
 
     # ensure "this" is taking slot zero
     constructor.local('another')
     assert_equal(constructor.local('this'), 0)
     assert_equal(constructor.local('another'), 1)
+
+    obj = load_and_construct(@class_name, cb)
+    assert_equal(obj, obj.get_self)
   end
 
   def test_native_method
-    cb = @builder.public_class('Foo', @builder.object);
+    cb = @builder.public_class(@class_name, @builder.object);
     
     body_called = false
     cb.public_native_method("yoohoo") {body_called = true}
 
     assert !body_called
 
-    cons = cb.public_constructor {aload local 'this'; invokespecial object, '<init>', [void]; returnvoid}
-
-    class_bytes = cb.generate
-    File.open('Foo.class', 'w') {|f| f.write(class_bytes)}
-    cls = JRuby.runtime.jruby_class_loader.define_class('Foo', class_bytes.to_java_bytes)
-
-    obj = cls.new_instance
+    dummy_constructor(cb)
+    obj = load_and_construct(@class_name, cb);
+    
     # expect NativeException (UnsatisfiedLinkError)
     assert_raises(NativeException) {obj.yoohoo}
   end
