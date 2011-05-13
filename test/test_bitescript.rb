d5a7e4dae768cf0165728c185a450fb61adde301
Nick Howard <ndh@baroquebobcat.com>
2011-05-13 12:18:44 -0600

    default is now dynamic depending on java version used

--- test/test_bitescript.rb
+++ test/test_bitescript.rb
@@ -4,8 +4,15 @@ require 'test/unit'
 require 'bitescript'
 
 class TestBitescript < Test::Unit::TestCase
+  def test_bytecode_defaults_to_current_version
+    spec_version = ENV_JAVA['java.specification.version']
+    expected_version = BiteScript.const_get("JAVA#{spec_version.gsub('.', '_')}")
+
+    assert_equal expected_version, BiteScript.bytecode_version
+  end
+
   def test_bytecode_version
-    assert_equal BiteScript::JAVA1_4, BiteScript.bytecode_version
+    
     [BiteScript::JAVA1_4, BiteScript::JAVA1_5, BiteScript::JAVA1_6].each do |ver|
       BiteScript.bytecode_version = ver
       assert_equal(ver, BiteScript.bytecode_version)
